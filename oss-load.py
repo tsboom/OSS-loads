274311ecd841c61c50869a153e4426cd0fff6e3d
Tiffany Schoneboom <thschone@umd.edu>
2018-09-21 10:36:30 -0400

    oss-load.py works but with sqlplus feedback problem.

--- oss-load.py
+++ oss-load.py
@@ -21,15 +21,19 @@ next(reader, None)
 
 # functions that do basic checks to figure out which fields are provided
 def is_barcode(value):
-    regex = r"[0-9]{14}"
+    regex = r"[0-9]{12,14}"
     if re.search(regex, value):
         return True
+    else:
+        return False
 
 def is_rmst(value):
     regex = r"R[0-9]{2}M[0-9]{2}S[0-9]{2}T[0-9]{2}"
     #regex = r"Z[0-9]{2}M[0-9]{2}S[0-9]{2}T[0-9]{2}"
     if re.search(regex, value):
         return True
+    else:
+        return False
         
 # variable to describe the type of CSV after it is checked
 csv_type = ""
@@ -44,6 +48,7 @@ pp = pprint.PrettyPrinter(indent=4)
 for row in csv_dict:
     print ".........."
     pp.pprint(row) 
+    #pdb.set_trace()
     print "\n\nrow length is: " + str(len(row)) +"\n\n"
     if len(row) > 5:
         print "Error: There are too many columns. Please check the data and try again."
@@ -56,19 +61,21 @@ for row in csv_dict:
             print "Error: Please check your size and height values. The third column should be a letter code for size."
     elif len(row) == 4 and is_barcode(row[0]):
         csv_type = "barcode, title, author, call number"
-    elif len(row == 3 and is_barcode(row[0]) and is_rmst(row[1])):
+    elif len(row) == 3 and is_barcode(row[0]) and is_rmst(row[1]):
         csv_type = "barcode, RMST, title"
     else: 
         print "Error: There is a problem with the CSV file. Please check the data and try again."
     f.close()
     break
+    quit()
     
 print "CSV type: " + csv_type + "\n\n" 
 
 # set up SQL file
 # name the SQL file the same name as the CSV, but with a .sql extension
 filename_no_ext = os.path.splitext(csv_file)[0]
-sql_file = filename_no_ext + '.sql'
+# remove spaces from sqlfile name
+sql_file = filename_no_ext.replace(" ", "")  + '.sql'
 f = open(sql_file, "w")
 print "sql_file: " + sql_file + "\n\n"
 
@@ -100,7 +107,7 @@ def run_sql_queries():
     session = Popen(['sqlplus', '-s', login_string], stdin=PIPE, stdout=PIPE, stderr=PIPE)
     session.stdin.write('set feedback off\n')
     print "Executing SQL file..."
-    session.stdin.write('@'+ filename_no_ext +';')
+    session.stdin.write('@'+ filename_no_ext +';\n')
     ## communicate results to stdout
     #return session.communicate(i)
     return session
@@ -115,16 +122,25 @@ except ValueError as e:
     print('There is an error with the sql file')
 else:
     print "Opening SQL Plus"
-    # opens Sql plus session and saves results
+    # open Sql plus session and saves results
     session = run_sql_queries()
-    query_result, error_messages = session.communicate() 
-    print "---\n\nSQL Plus results:\n\n" + str (query_result) + "\n\n"
-    print "---\n\nError messages:\n\n" + str(error_messages) + "\n\n"
-   
+    # capture output of sqlplus subprocess
+    
+    errors = []
+
+    while True:
+        line = session.stdout.readline()
+        if line != '':
+            errors.append(line.rstrip())
+            print errors[-1]
+            session.stdin.write('\n')
+        else:
+ 	    print "break happened"
+            pdb.set_trace()
+ 	    break
+    
     # commit changes
-    pdb.set_trace() 
-    session.stdin.write('commit;')
+    session.stdin.write('commit;\n')
 finally:
     print "\n\nExiting the database\n\n"
-    session.stdin.write('quit;')
-
+    session.stdin.write('quit;\n')
